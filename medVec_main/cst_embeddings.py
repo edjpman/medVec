8157dc614bce9ac678f710f538eced3bbb649ae1
Ethan <96897006+edjpman@users.noreply.github.com>
2025-05-05 22:59:33 -0700

    Resolving local dependency issues and adding code

--- medVec_main/cst_embeddings.py
+++ medVec_main/cst_embeddings.py
@@ -1,20 +1,161 @@
 
-class CustomBERT:
-
-    def __init__(self):
-        pass
-
-
-    ##Function to enstantiate a base version of ClinicalBert
-    def bertInst(self):
-        pass
-
-    ##Saves the custom model as a local memory object
-    def cBert_local(self):
-        pass
+from transformers import AutoTokenizer, AutoModel
+import torch
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 
-    ##Saves the custom model as an external file that can be loaded in later
-    def cBert_ext(self):
-        pass
+class cBERTbase:
+
+
+    '''
+
+    ddddd..only hugging face models allowed at the moment
+
+    '''
+
+    def __init__(self, inputs, model_name="medicalai/ClinicalBERT"):
+        self.inputs = inputs
+        self.model = AutoModel.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+
+    def mv_tokenizer(self,train_texts,dev_texts,test_texts,train_labels,dev_labels,test_labels,repr='pooled'):
+      '''
+
+      Descriptor here
+
+      '''
+
+      #Check if data is in a list format if not it converts to a Python list object
+      if not isinstance(train_texts, list):
+        train_texts = train_texts.tolist()
+      if not isinstance(dev_texts, list):
+          dev_texts = dev_texts.tolist()
+      if not isinstance(test_texts, list):
+          test_texts = test_texts.tolist()
+      if not isinstance(train_labels, list):
+          train_labels = train_labels.tolist()
+      if not isinstance(dev_labels, list):
+          dev_labels = dev_labels.tolist()
+      if not isinstance(test_labels, list):
+          test_labels = test_labels.tolist()
+
+      #Creates dictionary structure for datasplits
+      data_splits = {
+        'train': {'texts': train_texts, 'labels': train_labels},
+        'dev':   {'texts': dev_texts,   'labels': dev_labels},
+        'test':  {'texts': test_texts,  'labels': test_labels}
+      }
+
+      #Initializes an empty dictionary to store the embeddings and labels within
+      embd_splits = {}
+
+
+      #Loops through each of the split groups
+      #---First tokenizing the text
+      #---Instantiates BERT and initializes them as the model inputs
+      #---Creates the sentence embeddings and adds them to the empty dictionary by split group for raw use or classification tasks
+      for split in ['train','dev','test']:
+        texts = data_splits[split]['texts']
+        labels = data_splits[split]['labels']
+
+        inputs = self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True)
+        self.inputs = inputs
+        embeddings = self.bertInst(repr=repr)
+
+        embd_splits[split] = {
+            'embeddings': embeddings,
+            'labels': torch.tensor(labels, dtype=torch.long)
+        }
+
+      return embd_splits
+
+
+    def bertInst(self,repr='pooled'):
+        '''
+
+        Descriptor here
+
+        '''
+
+        #Assigns the IDs and Attention mask features of the tokenized inputs to separate objects
+        input_ids = self.inputs['input_ids']
+        attention_mask = self.inputs['attention_mask']
+
+        #Passes the inputs through the specified BERT model assigning the last hidden state to an object
+        with torch.no_grad():
+          outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+          last_hidden_state = outputs.last_hidden_state
+
+        #Conditional logic to specify the desired representation of the sentence embedding
+        if repr == 'cls':
+          cls_embedding = last_hidden_state[:, 0, :]
+          return cls_embedding
+        elif repr == 'pooled':
+          sentence_embedding = torch.mean(last_hidden_state, dim=1)
+          return sentence_embedding
+        else:
+          return ValueError('Expected "cls" or "pooled"')
+
+
+
+    def ttd_splits(self,dataset,x_col,y_col,train_prop=0.7,dev_prop=0.2,test_prop=0.1):
+
+      '''
+      returns: X_train, X_dev, X_test, y_train, y_dev, y_test
+
+      '''
+
+      #Raises an error if the sum of the three is not equal to 1 (i.e. 100% of data)
+      if not np.isclose(train_prop + dev_prop + test_prop, 1.0):
+        raise ValueError('The sum of the proportions must sum to 1!')
+
+
+      #Automatically balances the proportions for the incorporation of a dev set
+      modified_dev_prop = dev_prop/(1-test_prop)
+
+      #This splits it into the true test set (i.e. 10% default)
+      X_train, X_test, y_train, y_test = train_test_split(dataset[x_col], dataset[y_col], test_size=test_prop, random_state=42)
+      #This splits it into the true train size (i.e. 70% default) and dev size (i.e. 20% default)
+      X_train, X_dev, y_train, y_dev = train_test_split(X_train, y_train, test_size=modified_dev_prop, random_state=42)
+
+      return X_train, X_dev, X_test, y_train, y_dev, y_test
+
+
+
+class BERTdataset(Dataset):
+
+  '''
+
+  Wraps the embeddings into a `PyTorch` compatible dataset for efficient loading and batching during training.
+
+  Takes inputs such as:
+
+  embeddings = [
+    [0.1, 0.2, 0.3],
+    [0.4, 0.5, 0.6]
+  ]
+  labels = [0, 1]
+
+  dataset[1] --- ([0.4, 0.5, 0.6], 1)
+
+
+  If a batch of 2 is set throguh DataLoader() method:
+
+  ([[0.4, 0.5, 0.6], [0.1, 0.2, 0.3] ], [1, 0])
+
+  '''
+
+
+  def __init__(self, embeddings, labels):
+     self.embeddings = embeddings
+     self.labels = labels
+
+  def __len__(self):
+    return len(self.labels)
+
+  def __getitem__(self, idx):
+    return self.embeddings[idx], self.labels[idx]
 
--- medVec_main/preprocessing.py
+++ medVec_main/preprocessing.py
@@ -1,28 +1,95 @@
 
-
-
-
-##Imports go here
-
-
+import re
+import spacy
+from datasets import load_dataset
+import pandas as pd
 
 class mvPreproc:
 
+    '''
+
+    ddddd
+
+    '''
+
+    #Initialized spacy model for noun parsing
     def __init__(self):
-        pass
+      self.nlp = spacy.load('en_core_web_sm')
+
+    @staticmethod
+    def ds_load():
+      '''
+
+      Method loads in the two datasets from `Hugging Face`.
+
+      Inputs: No inputs required, just call the method.
+
+      Returns: patient dataset, medical definition dataset as pandas dataframes.
 
 
-    ##Future function for specific sampling methods to be applied
-    def sampling(self):
-        pass
+      '''
+      #Datasets from Hugging Face
+      patient_ds = load_dataset('gretelai/symptom_to_diagnosis')
+      med_ds = load_dataset('celikmus/mayo_clinic_symptoms_and_diseases_v1')
+      patient_ds = pd.DataFrame(patient_ds['train'])
+      patient_ds = patient_ds.rename(columns={'output_text':'label','input_text':'text'})
+      med_ds = pd.DataFrame(med_ds['train'])
+      return patient_ds, med_ds
 
-    ##Text length specification for more effective training on clinical definitions
-    def text_len_spec(self):
-        pass
+    @staticmethod
+    def clean_text(text):
+      '''
+
+      Method cleans text data by removing URLs, citation marks, and HTML tagging.
+
+      Inputs: The text to be cleaned.
+
+      Returns: Cleaned version of the text.
 
 
-    ##Handles the tokenization process for custom embeddings or input text
-    def mv_tokenizer(self):
-        pass
+      '''
+      #URL removal
+      text = re.sub(r'http[s]?://[^\s\)\]]+', '', text)
+      #Citation removal
+      text = re.sub(r'\[\d+\]', '', text)
+      text = re.sub(r'\([^)]*\d{4}[^)]*\)', '', text)
+      #HTML removal
+      text = re.sub('<.*?>', '', text)
+      return text
+
+    def noun_phrases(self,text):
+      '''
+
+      Method to return noun phrases from text data. This is specific to English only.
+
+
+      Inputs: The text to parse nouns from.
+
+      Returns: Noun phrases from input text separaed by a |.
+
+
+      '''
+      #Leverages the initialized spacy model
+      doc = self.nlp(text)
+      return ' | '.join(chunk.text for chunk in doc.noun_chunks)
+
+
+    @staticmethod
+    def feat_map(label_map,dataset,col):
+      '''
+
+      Method maps new labels for categories within a feature of a dataset.
+
+      Example of expected mapping
+      label_map = {'cat': 0, 'dog': 1, 'rabbit': 2}
+
+      Inputs: A dictionary of the old-new mapped labels, the dataset to which the mapping is applied to, the name of the column to apply the mapping to.
+
+      Returns: The dataset with the transformed column.
+
+
+      '''
+      dataset[col] = dataset[col].map(label_map)
+      return dataset
 
 
